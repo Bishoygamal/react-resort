7bf45ed2cff192d9ff78ff45b7b9264ad687b128
Bishoy Gamal <beshoy.ot@gmail.com>
2020-01-16 10:20:03 +0200

    update routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import singleRoom from './pages/singleRoom';
+import  Error from './pages/Error';
 
 import './App.css';
-
+import { Route ,Switch} from 'react-router-dom';
+import Navnar from './components/Navbar';
 function App() {
   return (
 <React.Fragment>
-  <h3></h3>
+  <Switch>
+  <Route exact path="/" component={Home} />
+  <Route exact path="/rooms/" component={Rooms} />
+  <Route exact path="/rooms/:slug" component={singleRoom} />
+  <Route component={Error} />
+  }
+  </Switch>
 </React.Fragment>
   );
 }
